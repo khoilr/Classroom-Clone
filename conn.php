115d542d00968fe7c5f40a3cad0d9e08a36dbdc3
Lê Công Minh Khôi <khoile2239@gmail.com>
2021-06-09 21:12:02 +0700

    change info

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
-$sever = "localhost";
-$username = "khoilr";
-$password = "khoikhoi";
-$database = "DoAnCuoiKi";
+$sever = "us-cdbr-east-04.cleardb.com";
+$username = "b66e857b169beb";
+$password = "d2e29911";
+$database = "heroku_7341053d835f019";
 // Create connection
 $conn = new mysqli($sever, $username, $password, $database);
 
